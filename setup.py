92cb6347b7949c0f2de420d4ed923c44b7c54528
Arvs007 <chowdhuryarve92@gmail.com>
2026-06-17 15:07:15 +0600

    jupyter nootbook work completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="yetzer",
+    author_email="chowdhuryarve@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
